6de3c8288bea791fcf3f509efba2188611d5e0d1
asahi0229 <asahiyuuto@asahiyuuto-no-MacBook-Air.local>
2019-02-18 18:40:07 +0900

    修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function() {
   var user_list = $('#user-search-result');
   var member_list = $('#chat-group-users');
 
-function appendUser(user){
+  function appendUser(user){
   var html = `<div class="chat-group-user clearfix">
                    <p class="chat-group-user__name">${user.name}</p>
                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -11,7 +11,7 @@ function appendUser(user){
   user_list.append(html);
 }
 
-function appendMember(user_id,user_name){
+  function appendMember(user_id,user_name){
   var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
   <input name='group[user_ids][]' type='hidden' value='${user_id}'>
   <p class='chat-group-user__name'>${user_name}</p>
