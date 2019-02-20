756b0e2d99aa3d001b2f8eabf10e5d2767c35b65
asahi0229 <asahiyuuto@asahiyuuto-no-MacBook-Air.local>
2019-02-20 18:30:35 +0900

    インデントを揃えた

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,32 +1,32 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
-  function buildHTML(message){
-    console.log(message)
-    var message = `<div class="message" data-message-id="${message.message_id}">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                      ${message.user_name}
+    function buildHTML(message){
+      console.log(message)
+      var message = `<div class="message" data-message-id="${message.message_id}">
+                      <div class="upper-message">
+                        <div class="upper-message__user-name">
+                        ${message.user_name}
+                        </div>
+                        <div class="upper-message__date">
+                        ${message.created_at}
+                        </div>
                       </div>
-                      <div class="upper-message__date">
-                      ${message.created_at}
-                      </div>
+                      <div class="lower-meesage">
+                      <p class="lower-message__content">
+                      ${message.content}
+                      </p>`
+      if (message.image){
+        var html =`${message}
+        <img src="${message.image}" class='lower-message__image'>
                     </div>
-                    <div class="lower-meesage">
-                    <p class="lower-message__content">
-                    ${message.content}
-                    </p>`
-    if (message.image){
-      var html =`${message}
-      <img src="${message.image}" class='lower-message__image'>
+                  </div>`
+      }else{
+        var html = `${message}
                   </div>
-                </div>`
-    }else{
-      var html = `${message}
-                </div>
-                </div>`
+                  </div>`
+      }
+      return html;
     }
-    return html;
-  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -43,6 +43,7 @@ $(document).on('turbolinks:load', function() {
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.form__message').val('');
+      // $('.form__message').remove();
     })
     .fail(function(){
       alert('error');
@@ -52,24 +53,21 @@ $(document).on('turbolinks:load', function() {
   $(function(){
     setInterval(update, 5000);
   });
-  function update(){
-    var lastmessage = $(".message").last().data('message-id');
-    var pathname= location.pathname;
-    $.ajax({
-      url: pathname,
-      type: 'GET',
-      data: { message: lastmessage},
-      dataType: 'json'
-    })
-    .done(function(data){
-      data.forEach(function(new_message){
-      var html = buildHTML(new_message);
-      $('.messages').append(html);
+    function update(){
+      var lastmessage = $(".message").last().data('message-id');
+      var pathname= location.pathname;
+      $.ajax({
+        url: pathname,
+        type: 'GET',
+        data: { message: lastmessage},
+        dataType: 'json'
       })
-    });
-    .fail(function(){
-    alert('error');
-    })
-  }
+      .done(function(data){
+        data.forEach(function(new_message){
+        var html = buildHTML(new_message);
+        $('.messages').append(html);
+        })
+      });
+    }
   })
 });
