5580498728a0391ee2b2636a82c07d56141ed54f
asahi0229 <asahiyuuto@asahiyuuto-no-MacBook-Air.local>
2019-02-21 11:22:20 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,7 +40,8 @@ $('#new_message').on('submit', function(e){
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
-    $('.form__message').val('');
+    $('form')[0].reset();
+
   })
   .fail(function(){
     alert('error');
